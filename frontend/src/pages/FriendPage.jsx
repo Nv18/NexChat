e0ec98c5a18b063f4088d381b17ecf13173e6622
Nikhil Kumar Singh <niksng101@gmail.com>
2025-07-09 21:56:52 +0530

    first commit

---
+++ frontend/src/pages/FriendPage.jsx
@@ -0,0 +1,43 @@
+import { Link } from "react-router";
+import FriendCard from "../components/FriendCard";
+import NoFriendsFound from "../components/NoFriendsFound";
+import { UsersIcon } from "lucide-react";
+import { useQuery } from "@tanstack/react-query";
+import { getUserFriends } from "../lib/api.js";
+
+const FriendPage = () => {
+
+    const { data: friends = [], isLoading: loadingFriends } = useQuery({
+    queryKey: ["friends"],
+    queryFn: getUserFriends,
+  });
+
+  return (
+    <div>
+        <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between gap-4">
+          <h2 className="text-2xl sm:text-3xl font-bold tracking-tight">Your Friends</h2>
+          <Link to="/notifications" className="btn btn-outline btn-sm">
+            <UsersIcon className="mr-2 size-4" />
+            Friend Requests
+          </Link>
+        </div>
+
+        {loadingFriends ? (
+          <div className="flex justify-center py-12">
+            <span className="loading loading-spinner loading-lg" />
+          </div>
+        ) : friends.length === 0 ? (
+          <NoFriendsFound />
+        ) : (
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+            {friends.map((friend) => (
+              <FriendCard key={friend._id} friend={friend} />
+            ))}
+          </div>
+        )} 
+
+    </div>
+  )
+}
+
+export default FriendPage
